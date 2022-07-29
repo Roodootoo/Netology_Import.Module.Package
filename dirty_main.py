adae8e679ef813f25df8a111c21b422eaaff595e
Roodootoo <elena@pyzhov.ru>
2022-07-29 14:28:19 +0300

    Done

---
+++ dirty_main.py
@@ -0,0 +1,10 @@
+from application.salary import *
+from application.db.people import *
+import datetime
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    print(datetime.datetime.now().strftime('%d/%m/%Y'))
+
+
